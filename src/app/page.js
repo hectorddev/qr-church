2596da94df76bac5e4ee793439d606c7a98ea9b0
Hector Marquez <hectordmv21@gmail.com>
2025-04-08 11:38:20 -0400

    fix: escapar comillas

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <div className="verse-container">
       <p className="verse-text">
-        "Porque donde están dos o tres congregados en mi nombre, allí estoy yo
-        en medio de ellos."
+        &ldquo;Porque donde están dos o tres congregados en mi nombre, allí
+        estoy yo en medio de ellos.&rdquo;
       </p>
       <p className="verse-reference">Mateo 18:20</p>
     </div>
